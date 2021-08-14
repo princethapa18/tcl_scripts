4a2f636b4af34d598137c3ec79ecf3802827227e
Prince Thapa <princethapa11@protonmail.com>
2021-08-14 21:12:14 +0530

    Update args.tcl

--- args.tcl
+++ args.tcl
@@ -1,18 +1,24 @@
-# taking arguments in tcl 
+#!/usr/bin/tclsh
 
-# argc means no. of arguments passed
-# argv0 name of the script
-# arrgv array containing arguments
+if 0 {
+   this is a multiline comment
+   ********* Working with arguments in tcl ******************
+   argc means no. of arguments passed
+   argv0 name of the script
+   arrgv array containing arguments
+}
 
 puts "There are $argc arguments to this script"
 puts "The name of this script is $argv0"
 
 if {$argc > 0} {
-   puts "The other arguments are: $argv" 
+   puts "The arguments are: $argv" 
 }    
 
 for {set i 0} {$i < [llength $argv]}  {incr i}  {
     puts "arg $i is [lindex $argv $i]"
 }
 
-
+foreach item $argv {
+  puts $item
+}
