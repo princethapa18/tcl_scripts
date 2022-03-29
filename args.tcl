6f49f1a054f44e5f16d114d9860dd275e523e9db
Prince Thapa <princethapa11@protonmail.com>
2022-03-29 16:11:22 +0530

    taking user as input   29 Mar 2022

--- args.tcl
+++ args.tcl
@@ -1,24 +1,40 @@
-#!/usr/bin/tclsh
+# 29 Mar 2022
+# run like this tclsh chown.tcl <username>
+# tcl script to change the ownership of file
 
-if 0 {
-   this is a multiline comment
-   ********* Working with arguments in tcl ******************
-   argc means no. of arguments passed
-   argv0 name of the script
-   arrgv array containing arguments
+set parentPath [exec pwd]
+cd $parentPath
+
+#set user gns137
+#set user thapa
+
+set user [lindex $argv 0]
+puts "user name : $user\n"
+
+puts " $parentPath ownership changed\n"
+#exec chown <user> $parentPath
+exec chown $user $parentPath
+
+set childList [exec ls]
+
+proc process {childList parentPath user} {  
+   foreach item $childList {
+	cd $parentPath      
+        if {[file isdirectory $item]} {               	
+		set childPath [file join $parentPath $item]
+		puts " $childPath ownership changed\n"
+                exec chown $user $item
+
+		cd $childPath 
+                set childs [exec ls]
+                puts " Going inside directory: $childPath\n"
+        	process $childs $childPath $user
+    	} elseif {[file isfile $item]} {
+                set filename [file join $parentPath $item]           
+		puts " $filename ownership changed\n"
+		exec chown $user $item       
+        }
+   }
 }
 
-puts "There are $argc arguments to this script"
-puts "The name of this script is $argv0"
-
-if {$argc > 0} {
-   puts "The arguments are: $argv" 
-}    
-
-for {set i 0} {$i < [llength $argv]}  {incr i}  {
-    puts "arg $i is [lindex $argv $i]"
-}
-
-foreach item $argv {
-  puts $item
-}
+process $childList $parentPath $user
